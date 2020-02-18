38603e7fe3dc02aa560ecdefb434495f7e44ce78
fortinv-test <fortinv@gmail.com>
2020-02-18 18:29:16 +0000

    Add a class for math functions

---
+++ src/lib/compute.py
@@ -0,0 +1,16 @@
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+        pass
